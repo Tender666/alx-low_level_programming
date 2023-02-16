439816e12706846f2ad63dc4f0d3253a936499f7
Tender666 <qwecisammy@gmail.com>
2023-02-16 15:42:02 -0800

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - as said , I need to print a line using puts , not print
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
