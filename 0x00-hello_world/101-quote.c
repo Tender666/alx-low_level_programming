047447206efc14aef767da9487fc9676ad8f3112
Tender666 <qwecisammy@gmail.com>
2023-02-16 17:05:52 -0800

    101 quote c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return:must be 1
+ *
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
