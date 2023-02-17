9b32cfff7e34d09ef044d7a0af6e122103c3d7f2
Tender666 <qwecisammy@gmail.com>
2023-02-16 17:30:05 -0800

    101 quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
