1f92cd6d158faf2cc49d38b8e5a5dc5eb8115799
Tender666 <qwecisammy@gmail.com>
2023-02-24 12:57:23 -0800

    add twointegerss

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @x: first integer
+* @y: second integer
+* Return: addition of x and y
+*/
+int mul(int x, int y)
+{
+	return (x * y);
+}
