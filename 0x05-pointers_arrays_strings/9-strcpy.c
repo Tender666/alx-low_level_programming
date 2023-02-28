ed249c0fb6ba2ad14bfa04817770be0263d7618b
Tender666 <qwecisammy@gmail.com>
2023-02-27 17:14:14 -0800

    stropy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: destination buffer to copy to
+ * @src: source buffer to copy from
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	dest[a] = '\0';
+	return (dest);
+}
+
